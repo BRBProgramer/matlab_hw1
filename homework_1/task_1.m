2b50185fdd5a5e59aaaeb588e59bd7cbdbfcb174
BRBProgramer <barrybal3rd@gmail.com>
2024-09-27 15:46:58 +0000

    we solved the first task, pat on the back for me

---
+++ homework_1/task_1.m
@@ -0,0 +1,19 @@
+ % Author Name: Barry Ballurio
+% Email: ballur39@rowan.edu
+% Course: MATLAB Programming - Fall 2024
+% Assignment: Homework 1
+ 
+% a) Create a variable `radius` and assign it a value of 5.
+% b) Calculate the area of a circle with this radius and store it in a variable called `area`.
+% c) Calculate the circumference of the circle and store it in a variable called `circumference`.
+% d) Display the results with appropriate labels.
+
+%Completed File: 
+
+r = 5;
+%res = pi *r^2
+%fprintf("The area of the circle is %area",res);
+
+res = 2 *pi *r
+fprintf("The Circumference of the circle is %Circumference", res)
+
